b5f9f21450455c244ab995881a5c8461d935690d
YuqianSong7 <125743687+YuqianSong7@users.noreply.github.com>
2023-04-23 23:29:52 +0200

    Create compareDot.py

---
+++ compareDot.py
@@ -0,0 +1,7 @@
+import networkx as nx
+from networkx import *
+
+G1 = nx_pydot.read_dot("hypothesis_problemPin.dot")
+G2 = nx_pydot.read_dot("learnlib-final_problemPin.dot")
+
+print(is_isomorphic(G1, G2, node_match=None, edge_match=None))
